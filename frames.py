45aa8b79fd2a35fc80c359ab5fdf89358ddb9c12
Luiz Felipe Domingues <lfsando@gmail.com>
2019-04-29 22:17:16 -0300

    Remove CLI file. CLI on main file

--- frames.py
+++ frames.py
@@ -2,53 +2,83 @@ import cv2
 from PIL import Image
 import numpy as np
 
+import click
 
-fpath = 'got.mkv'
+# input ( video  ) if image returns color average of image
+# output otherwise output
+# width
+# if height, must choose between cutting the video and/or specify frame capture time (every 2s), otherwise image will be gigantic if every frame and will take a long time
+# file ext
 
-video = cv2.VideoCapture(fpath)
+@click.command()
+@click.option('--filepath', help='Video/Image file.')
+@click.option('--width', default=800, help='Width of the output file')
+@click.option('--output', default='output', help='Output File')
+# @click.option('--height', )
+# @click.option('--extension')
+def main(filepath, width, output):
+    click.echo("""
+    Average color for specified frames and returns a representation of the data. 
+    """)
+    average = AverageFrameColor(filepath, width, output)
+
+class AverageFrameColor:
+    def __init__(self, filepath, width, output):
+        self.filepath = filepath
+        self.width = width
+        self.output = output
+        print(filepath, width, output)
+
+if __name__ == '__main__':
+    main()
+
+# fpath = 'got.mkv'
+
+# video = cv2.VideoCapture(fpath)
 
 
-frame_count = 0
-colors = []
-print('Averaging ', fpath)
-while True:
-    try:
-        ret, frame = video.read()
+# frame_count = 0
+# colors = []
+# print('Averaging ', fpath)
+# while True:
+#     try:
+#         ret, frame = video.read()
 
-        r_sum, g_sum, b_sum = (0, 0, 0)
-        px_count = 0
+#         r_sum, g_sum, b_sum = (0, 0, 0)
+#         px_count = 0
         
-        if frame_count % 48 == 0:
-            print()
-            for _row in frame:
-                for px in _row:
+#         if frame_count % 48 == 0:
+#             print()
+#             for _row in frame:
+#                 for px in _row:
                     
-                    r, g, b = px
-                    r_sum += r
-                    g_sum += g
-                    b_sum += b
-                    px_count += 1
-            r_med = int(round(r_sum / px_count))
-            g_med = int(round(g_sum / px_count))
-            b_med = int(round(b_sum / px_count))
-            colors.append([r_med, g_med, b_med])
-        print('|', end='')
+#                     r, g, b = px
+#                     r_sum += r
+#                     g_sum += g
+#                     b_sum += b
+#                     px_count += 1
 
-        frame_count += 1
-    except KeyboardInterrupt:
-        break
-    except Exception as e:
-        print(e)
-        break
+#             r_med = int(round(r_sum / px_count))
+#             g_med = int(round(g_sum / px_count))
+#             b_med = int(round(b_sum / px_count))
+#             colors.append([r_med, g_med, b_med])
+#         print('|', end='')
 
-width = 1920
-n_frames = len(colors)
-frames = []
-for color in colors:
-    frames.append([color]*width)
-im = np.array(frames).reshape(n_frames, width, 3)
+#         frame_count += 1
+#     except KeyboardInterrupt:
+#         break
+#     except Exception as e:
+#         print(e)
+#         break
 
-result = Image.fromarray(im.astype('uint8'))
+# width = 1920
+# n_frames = len(colors)
+# frames = []
+# for color in colors:
+#     frames.append([color]*width)
+# im = np.array(frames).reshape(n_frames, width, 3)
 
-result.show()
-result.save(f'output.jpeg', 'JPEG')
+# result = Image.fromarray(im.astype('uint8'))
+
+# result.show()
+# result.save(f'output.jpeg', 'JPEG')
